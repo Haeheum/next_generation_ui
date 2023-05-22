9578d27a549abf68951173b4fbcac9f5ba3a916c
Haeheum <hammizzang@gmail.com>
2023-05-23 06:42:55 +0900

    Feat: Add orb

--- lib/title_screen/title_screen_ui.dart
+++ lib/title_screen/title_screen_ui.dart
@@ -17,11 +17,13 @@ class TitleScreenUi extends StatelessWidget {
     required this.difficulty, // Edit from here...
     required this.onDifficultyPressed,
     required this.onDifficultyFocused,
+    required this.onStartPressed,
   });
 
   final int difficulty;
   final void Function(int difficulty) onDifficultyPressed;
   final void Function(int? difficulty) onDifficultyFocused;
+  final VoidCallback onStartPressed;
 
   @override
   Widget build(BuildContext context) {
@@ -57,7 +59,7 @@ class TitleScreenUi extends StatelessWidget {
               alignment: Alignment.bottomRight,
               child: Padding(
                 padding: const EdgeInsets.only(bottom: 20, right: 40),
-                child: _StartBtn(onPressed: () {}),
+                child: _StartBtn(onPressed: onStartPressed),
               ),
             ),
           ),
